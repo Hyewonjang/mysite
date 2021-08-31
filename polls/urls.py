bae12f43fbc75a8d7b4a508e8679e74d09004cf9
Hyewonjang <hyewon5254295@gmail.com>
2021-09-01 01:04:33 +0900

    fourth commit

--- polls/urls.py
+++ polls/urls.py
@@ -6,10 +6,19 @@ app_name = 'polls' # 다른 app에서도 같은 name 쓸 수 있으므로 app_na
 urlpatterns = [
     # ex /polls/
     path('', views.index, name='index'),
+    # path('', views.IndexView.as_view(), name='index'),
+    
     # ex /polls/5/
     path('<int:question_id>/', views.detail, name='detail'), # 괄호 내 첫번째 ''은 urlpattern으로 urlpattern에 맞는 url의 경우 두번째에 적혀있는 해당 views를 호출하겠다는 의미이며, name은 urlpattern을 직접적으로 쓰지 않고 이를 가리킬 수 있는 것이다.
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'), # pk란? db 내의 하나의 열, 즉 하나의 데이터를 구분할 수 있는 값(중복 없음)
+    
     # ex /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'), # question_id는 views.results의 parameter인 question_id와 동일하다. 
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
+    
     # ex /polls/5/vote
     path('<int:question_id>/vote/', views.vote, name='vote'),  # <int:변수>/ Django에서 지원하는 url패턴
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,15 @@
 # from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.template import loader
+from django.template import context, loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 # 각 view는 HttpResponse 객체 또는 Http404 중 하나를 무조건 반환한다.
+# Class view는 다른 말로 generic view : django에서 미리 제공해주는 것이 많아 python 코드보다 더 적은 코드로 같은 기능을 만들 수 있다. 하지만 장고에 익숙해질 때까지는 함수 기반 view 작성을 추천
 def index(request):
     # 1
     # return HttpResponse("Hello, World.")
@@ -29,6 +32,15 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+#     context_object_name = 'latest_question_list' # context에 넘겨주는 이름이 모델이름과 다를 때 context_object_name 설정 후 get_queryset함수를 통해 필요한 데이터를 다시 작성한다.
+
+#     def get_queryset(self):
+#         """Return the last five published questions."""
+#         return Question.objects.order_by('-pub_date')[:5]
+
+
 def detail(request, question_id):
     # 1
     # return HttpResponse("You're looking at question %s." % question_id)
@@ -44,28 +56,46 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question':question})
 
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
+
+
 def results(request, question_id):
-    response = "You're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    # 1
+    # response = "You're looking at the results of question %s"
+    # return HttpResponse(response % question_id)
+
+    # 2
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question' : question})
+
+# class ResultsView(generic.DetailView):
+#     model = Question # 해당 템플릿에서 사용할 데이터 모델
+#     template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     # 1
     # return HttpResponse("You're voting on question %s." % question_id)
 
     # 2
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice']) # request(받은 정보) 중 method가 POST 인 것 중 name이 choice인 선택된 설문의 ID를 문자열로 반환한다.
-    except (KeyError, Choice.DoesNotExist): # choice가 없으면 detail 페이지 달라짐.
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {  
-            'question':question,
-            'error_message':"You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after succesffully deal with Post data.
-        # This prevents data from being posted twice
-        # if user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question_id, id))) # POST와 ResponseRedirect는 한 세트임. reverse의 경우 url을 하드코딩 즉, urlpattern에 맞게 적지 않기 위하여 reverse 사용하고 'polls:results' urls.py에서 설정한 name 사용한 것임.
+    # if request.method == 'GET': # views에는 get, post 방식 모두 사용할 수 있음.
+        # do_something()
+    # elif request.method == 'POST':
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice']) # request(받은 정보) 중 method가 POST 인 것 중 name이 choice인 선택된 설문의 ID를 문자열로 반환한다.
+        except (KeyError, Choice.DoesNotExist): # choice가 없으면 detail 페이지 이동(detail 페이지 내용 바뀜).
+            # Redisplay the question voting form.
+            return render(request, 'polls/detail.html', {  
+                'question':question,
+                'error_message':"You didn't select a choice.",
+            })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            # Always return an HttpResponseRedirect after succesffully deal with Post data.
+            # This prevents data from being posted twice
+            # if user hits the Back button.
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) # POST와 ResponseRedirect는 한 세트임. reverse의 경우 url을 하드코딩 즉, urlpattern에 맞게 적지 않기 위하여 reverse 사용하고 'polls:results' urls.py에서 설정한 name 사용한 것임.
